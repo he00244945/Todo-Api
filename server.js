4b69e3a358f51cc97f32c330be5babf2ba896346
he00244945 <he00244945@gmail.com>
2016-10-02 13:04:57 +0800

    Add todo model and POST

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var db = require('./db.js');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -47,7 +48,12 @@ app.get('/todos/:id', function(req, res){
 
 app.post('/todos', function(req, res){
 	var body = _.pick(req.body, 'description', 'completed');
-
+	db.todo.create(body).then(function (todo){
+		res.json(todo.toJSON());
+	}, function (e){
+		res.status(400).json(e);
+	});
+	/*
 	//trim 把一個字串的前後空白去掉
 	if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
 		return res.status(400).send();
@@ -60,6 +66,7 @@ app.post('/todos', function(req, res){
 	todos.push(body);
 
 	res.json(body);
+	*/
 });
 
 app.delete('/todos/:id', function(req, res){
@@ -101,6 +108,11 @@ app.put('/todos/:id', function(req, res){
 
 });
 
-app.listen(PORT, function () {
+
+db.sequelize.sync().then( function(){
+	app.listen(PORT, function () {
 	console.log('Express listening on port' + PORT + ' !');
+	});
 });
+
+
